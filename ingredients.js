b00f40b16193597796f40bb6cc7529d7b357f140
Piotr <bartnic1@gmail.com>
2018-01-25 17:05:18 +0000

    Adds ingredients.js

--- ingredients.js
+++ ingredients.js
@@ -6,12 +6,12 @@ while(i < ingredients.length){
   console.log(ingredients[i]);
   i++;
 }
-console.log("----------");
+console.log("-----------");
 // Write a for loop that prints out the contents of ingredients:
 for(var i = 0; i < ingredients.length; i++){
   console.log(ingredients[i]);
 }
-console.log("----------");
+console.log("-----------");
 // Write any loop (while or for) that prints out the contents of ingredients backwards:
 for(var i = ingredients.length - 1; i >= 0; i--){
   console.log(ingredients[i]);
